4a3c9666c27afd3d3d0231b7be8a1dac0793c7fb
Mike Esposito <92817889+MEspositoRonin@users.noreply.github.com>
2023-05-02 16:05:29 -0400

    [INT-1655] Updated QueryLoggerInstrumentation to catch validation errors (#427)
    * Updated QueryLoggerInstrumentation to catch validation errors and log them as errors
    
    * Switched to filter based approach
    
    ---------
    
    Co-authored-by: Mike Esposito <Michael.Esposito@EspoEnterprises.com>

--- src/main/kotlin/com/projectronin/interop/proxy/server/instrumentation/QueryLoggerInstrumentation.kt
+++ src/main/kotlin/com/projectronin/interop/proxy/server/instrumentation/QueryLoggerInstrumentation.kt
@@ -1,10 +1,13 @@
 package com.projectronin.interop.proxy.server.instrumentation
 
 import graphql.ExecutionInput
+import graphql.ExecutionResult
 import graphql.execution.instrumentation.SimpleInstrumentation
 import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters
+import graphql.validation.ValidationError
 import mu.KotlinLogging
 import org.springframework.stereotype.Component
+import java.util.concurrent.CompletableFuture
 
 /**
  * Provides instrumentation of the GraphQL request that results in logging out the query that was sent to the server.
@@ -20,5 +23,22 @@ class QueryLoggerInstrumentation : SimpleInstrumentation() {
         logger.info { "Processing GraphQL query: ${executionInput.query}" }
 
         return executionInput
+    }
+
+    /*
+        Normally when there's a validation error on a GraphQL query, the error gets returned to the user and a warning
+        is logged.  We want to log them as an error, so that when someone sends a bad query, or tries to call a query
+        that doesn't exist, it'll get posted to #interops-errors and trigger a PagerDuty alert in Prod.
+     */
+    override fun instrumentExecutionResult(
+        executionResult: ExecutionResult?,
+        parameters: InstrumentationExecutionParameters?
+    ): CompletableFuture<ExecutionResult> {
+        executionResult?.let {
+            it.errors.filterIsInstance<ValidationError>().map { error ->
+                logger.error { "${error.errorType}: ${error.message}" }
+            }
+        }
+        return super.instrumentExecutionResult(executionResult, parameters)
     }
 }
--- src/test/kotlin/com/projectronin/interop/proxy/server/instrumentation/QueryLoggerInstrumentationTest.kt
+++ src/test/kotlin/com/projectronin/interop/proxy/server/instrumentation/QueryLoggerInstrumentationTest.kt
@@ -1,8 +1,15 @@
 package com.projectronin.interop.proxy.server.instrumentation
 
 import graphql.ExecutionInput
+import graphql.ExecutionResult
+import graphql.execution.instrumentation.SimpleInstrumentation
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters
+import graphql.validation.ValidationError
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.spyk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -15,5 +22,29 @@ class QueryLoggerInstrumentationTest {
 
         val output = QueryLoggerInstrumentation().instrumentExecutionInput(input, mockk())
         assertEquals(input, output)
+    }
+
+    @Test
+    fun `instrumentExecutionResult calls parent function`() {
+        val instrumentation = spyk(QueryLoggerInstrumentation(), recordPrivateCalls = true)
+        val executionResult = mockk<ExecutionResult>()
+        val instrumentationExecutionParameters = mockk<InstrumentationExecutionParameters>()
+
+        every { executionResult.errors } returns listOf(
+            mockk<ValidationError> {
+                every { message } returns "error"
+            }
+        )
+
+        mockkConstructor(SimpleInstrumentation::class)
+        every {
+            anyConstructed<SimpleInstrumentation>()
+                .instrumentExecutionResult(executionResult, instrumentationExecutionParameters)
+        } returns mockk()
+
+        instrumentation.instrumentExecutionResult(executionResult, instrumentationExecutionParameters)
+        verify(exactly = 1) {
+            (instrumentation as SimpleInstrumentation).instrumentExecutionResult(executionResult, instrumentationExecutionParameters)
+        }
     }
 }
