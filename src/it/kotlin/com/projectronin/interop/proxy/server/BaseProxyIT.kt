f12f955dd06bd1d6124e3e82b10d46cc60ec4941
Josh Smith <43484632+arssycro@users.noreply.github.com>
2024-02-26 10:10:57 -0600

    INT-2573: Cleanup Docker setup (#606)

--- src/it/kotlin/com/projectronin/interop/proxy/server/BaseProxyIT.kt
+++ src/it/kotlin/com/projectronin/interop/proxy/server/BaseProxyIT.kt
@@ -22,10 +22,6 @@ import com.projectronin.interop.tenant.config.data.model.TenantDO
 import org.junit.jupiter.api.AfterEach
 import org.ktorm.database.Database
 import org.ktorm.dsl.deleteAll
-import org.testcontainers.containers.DockerComposeContainer
-import org.testcontainers.containers.wait.strategy.Wait
-import java.io.File
-import java.time.Duration
 import java.time.LocalTime
 import java.time.ZoneId
 import java.util.stream.Stream
@@ -36,34 +32,6 @@ abstract class BaseProxyIT {
         fun tenantMnemonics(): Stream<String> {
             return Stream.of("epic", "cerner")
         }
-
-        val docker =
-            DockerComposeContainer(File(BaseProxyIT::class.java.getResource("/docker-compose-it.yaml")!!.file)).withEnv(
-                mapOf<String, String>(
-                    "AIDBOX_LICENSE_ID" to System.getenv("AIDBOX_LICENSE_ID"),
-                    "AIDBOX_LICENSE_KEY" to System.getenv("AIDBOX_LICENSE_KEY"),
-                    "AIDBOX_PORT" to "8888",
-                    "AIDBOX_CLIENT_ID" to "client",
-                    "AIDBOX_ADMIN_ID" to "admin",
-                    "AIDBOX_ADMIN_PASSWORD" to "secret",
-                    "AIDBOX_CLIENT_SECRET" to "secret",
-                    "AIDBOX_DEV_MODE" to "true",
-                    "PGPORT" to "5432",
-                    "PGHOSTPORT" to "5437",
-                    "AIDBOX_FHIR_VERSION" to "4.0.0",
-                    "PGHOST" to "database",
-                    "PGUSER" to "postgres",
-                    "POSTGRES_USER" to "postgres",
-                    "POSTGRES_PASSWORD" to "postgres",
-                    "POSTGRES_DB" to "devbox",
-                    "PGPASSWORD" to "postgres",
-                    "PGDATABASE" to "devbox",
-                    "box_features_validation_skip_reference" to "true",
-                ),
-            )
-                .withStartupTimeout(Duration.ofMinutes(10))
-                .waitingFor("proxy", Wait.forHealthcheck().withStartupTimeout(Duration.ofSeconds(120)))
-                .start()
     }
 
     protected val serverUrl = "http://localhost:8080"
